9d39d8c0f59228cd16af991b8b4d0a1d9a1032ce
Nandan Pai <nandanrp09@gmail.com>
2024-03-15 10:43:13 +0530

    Interval photos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,52 @@
-import { signIn, signOut, useSession } from "next-auth/react";
-import Head from "next/head";
-import Link from "next/link";
-import ImageAnnotation from "~/components/ImageButton/ImageButton";
-import Picture from "~/components/Camera/Picture";
-import { api } from "~/utils/api";
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import Webcam from 'react-webcam';
+import ImageAnnotation from '~/components/ImageButton/ImageButton';
 
-export default function Home() {
-  
+const videoConstraints = {
+  width: 1280,
+  height: 720,
+  facingMode: 'environment',
+};
+
+function App() {
+  const webcamRef = useRef(null);
+  const [photoLocation, setPhotoLocation] = useState(null); // Add this line
+
+  // const capture = useCallback(() => {
+    
+  // }, [webcamRef]);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (webcamRef.current) {
+        //@ts-ignore
+        const imageSrc = webcamRef.current.getScreenshot();
+        setPhotoLocation(imageSrc); // Update the photo location
+      }
+    },1000/30)
+
+    return () => {
+      clearInterval(intervalId);
+    }
+  })
+
   return (
-    <>
-      <div className="flex justify-center items-center w-full h-screen">
-        {/* <ImageAnnotation /> */}
-        <Picture />
-      </div>
-    </>
+    // <>
+    //   <div className="flex justify-center items-center w-full ">
+    //     <Webcam
+    //       audio={false}
+    //       height={720}
+    //       ref={webcamRef}
+    //       screenshotFormat="image/jpeg"
+    //       width={1280}
+    //       //@ts-ignore
+    //       videoConstraints={videoConstraints}
+    //     />
+        
+    //   </div>
+    //   {photoLocation && <img src={photoLocation} alt="captured" />}
+    // </>
+    <ImageAnnotation />
   );
 }
 
+export default App;
